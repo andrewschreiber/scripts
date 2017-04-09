7fee2fa79daf9df3699941f4a13cebccf023d79c
Andrew Schreiber <andrew.schreiber1@gmail.com>
2017-04-09 13:42:23 -0700

    Update asciiscript.sh

--- asciiscript.sh
+++ asciiscript.sh
@@ -52,6 +52,7 @@ if [[ $? != 0 ]] ; then
     # Install Homebrew
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
+    echo "Updating Homebrew. This may take a few minutes..."
     brew update
 fi
 echo "(Part 2) Success!"
