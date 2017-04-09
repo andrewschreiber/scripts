37ea41816572ef1f41d411b40b120b55b17d5ece
Andrew Schreiber <andrew.schreiber1@gmail.com>
2017-04-08 17:17:17 -0700

    Update asciiscript.sh

--- asciiscript.sh
+++ asciiscript.sh
@@ -22,19 +22,25 @@ echo "                  ▓▌                                                  
 tput sgr0
 echo
 echo "Setting up Gym. Estimated completion time: 5-10 minutes."
-
+echo "Your password will be requested to install Homebrew. 
+echo ">> Press ENTER to begin <<"
+echo 
 echo "**** OPENAI GYM SETUP SCRIPT ****"
 echo "Part 1) Create OpenAIGym folder"
 echo "*********************************"
+pause 1
 dir='OpenAIGym' 
-if [ ! -d dir ]; then
-     mkdir dir
+if [ ! -d $dir ]; then
+     mkdir $dir
 fi
-cd dir
+cd $dir
 
+echo
 echo "**** OPENAI GYM SETUP SCRIPT ****"
 echo "Part 2) Setup Homebrew"
 echo "*********************************"
+pause 1
+echo
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
@@ -43,9 +49,12 @@ else
     brew update
 fi
 
+echo
 echo "**** OPENAI GYM SETUP SCRIPT ****"
 echo "Part 3) Setup Python 3 / Conda"
 echo "*********************************"
+pause 1
+echo
 which -s conda 
 if [[ $? != 0 ]] ; then
     # Install conda
@@ -54,17 +63,29 @@ else
     conda update conda
 fi
 
+echo
 echo "**** OPENAI GYM SETUP SCRIPT ****"
 echo "Part 4) Install OpenAI Gym"
 echo "*********************************"
+pause 1
+echo
 pip install gym
 
-
+echo
 echo "**** OPENAI GYM SETUP SCRIPT ****"
 echo "Part 5) Install Gym Dependencies"
 echo "*********************************"
+pause 1
+echo
 brew install cmake boost boost-python sdl2 swig wget
 
+echo
+echo "**** OPENAI GYM SETUP SCRIPT ****"
+echo "Part 6) Installation complete! Testing a safety environment"
+echo "*********************************"
+pause 1
+echo
+open .
 
 echo "Detecting dependencies"
 echo "Need to install x,y,z"
