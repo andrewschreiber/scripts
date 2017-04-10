e34311cacf8370c39a46601833d8522f81557f5b
Andrew Schreiber <andrew.schreiber1@gmail.com>
2017-04-09 19:20:43 -0700

    Update asciiscript.sh

--- asciiscript.sh
+++ asciiscript.sh
@@ -112,9 +112,11 @@ echo; echo "**** OPENAI GYM SETUP SCRIPT ****"
 echo "Part 4 | Install OpenAI Gym"
 echo "(Tip) The pachi-py step takes a few minutes."
 echo "*********************************"; sleep 1; echo
-if [[ command_exists cmake !=0 ]] ; then 
+
+if [ command_exists cmake !=0 ] ; then 
     brew install cmake
 fi
+
 pip install 'gym[all]'
 
 echo "(Part 4) Success!"
