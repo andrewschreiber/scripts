822b592e1ff5dade25ebc4aaa37d6cc0a3fc5d8b
Andrew Schreiber <andrew.schreiber1@gmail.com>
2017-04-17 11:21:21 -0700

    Update gym_installer.sh

--- gym_installer.sh
+++ gym_installer.sh
@@ -53,9 +53,9 @@ if command_exists brew ; then
     echo "[CHECK] Homebrew already installed."
 else
     echo "Installing Homebrew. Enter your system password at prompt, then press enter."
-    tput setaf 4
-    echo "(Tip) After entering password, it takes awhile."
-    tput sgr0
+    tput smul
+    echo "[TIP] After entering password, it takes awhile."
+    tput rmul
     sleep 8
     echo "Downloading Homebrew..."
     # Install Homebrew
@@ -80,11 +80,11 @@ safe_brew_install cmake swig boost boost-python sdl2 wget
 
 echo; echo "[PART 1] Success!"
 echo; echo "**** OPENAI GYM SETUP SCRIPT ****"
-echo "[PART 2] Setup Python 3.6 / Conda"
-tput setaf 4
+echo "[PART 2] Setup Python 3.5 / Conda"
+tput smul
 echo "[TIP] Say 'yes' to each prompt that asks"
 echo "[TIP] Scroll down the license by holding enter"
-tput sgr0
+tput rmul
 echo "*********************************"; echo
 read -rsp $'>> Press enter to continue <<\n'
 
@@ -94,20 +94,20 @@ if command_exists conda ; then
     echo "Updating conda..."
     conda update conda
     case "$(python --version 2>&1)" in
-    *" 3.6"*)
-        echo "Using Python 3.6 already. Continuing..."
+    *" 3.5"*)
+        echo "Using Python 3.5 already. Continuing..."
         ;;
     *)
-        echo "Switching to Python 3.6 using Conda..."
+        echo "Switching to Python 3.5 using Conda..."
         
         set +e
-        conda create -n p36 python=3.6
+        conda create -n p35 python=3.5
         set -e
-        source activate p36
-        tput setaf 4
-        echo "(Tip) New terminal tabs/windows must run 'source activate p36' for Gym"
-        echo "(Tip) Add the above command to your .bash_profile for auto-activation"
-        tput sgr0
+        source activate p35
+        tput smul
+        echo "[TIP] New terminal tabs/windows must run 'source activate p35' for Gym"
+        echo "[TIP] Add the above command to your .bash_profile for auto-activation"
+        tput rmul
         read -rsp $'>> Press enter to continue <<\n'
         ;;
      esac
@@ -122,20 +122,19 @@ else
     # Dying around here for some reason, suddenly. Is it -nc flag? Or...?
     echo "Finished installing Miniconda"
     rm Miniconda3-latest-MacOSX-x86_64.sh
-    echo "Refreshed .bash_profile"
     source ~/.bash_profile
-    tput setaf 4
-    echo "[TIP] For Conda to work, restart Terminal after the script completes."
-    tput sgr0
+    tput smul
+    echo "[TIP] For Conda to work, type 'source ~/.bash_profile' after the script completes."
+    tput rmul
     read -rsp $'>> Press enter to continue <<\n'
 fi
 
 echo; echo "[PART 2] Success!"
 echo; echo "**** OPENAI GYM SETUP SCRIPT ****"
 echo "[PART 3] Install OpenAI Gym"
-tput setaf 4
+tput smul
 echo "[TIP] The pachi-py step takes awhile."
-tput sgr0
+tput rmul
 echo "*********************************"; sleep 1; echo
 
 pip install 'gym[all]'
@@ -164,7 +163,7 @@ python example_agent.py
 echo "[CHECK] Gym is working."
 echo "[PART 4] Success!"
 
-echo; echo
+echo
 tput setaf 54
 echo "  ███████╗ ██╗   ██╗  ██████╗  ██████╗ ███████╗ ███████╗ ███████╗    ██╗"
 echo "  ██╔════╝ ██║   ██║ ██╔════╝ ██╔════╝ ██╔════╝ ██╔════╝ ██╔════╝    ██║"
@@ -173,13 +172,16 @@ echo "  ╚════██║ ██║   ██║ ██║      ██║ 
 echo "  ███████║ ╚██████╔╝ ╚██████╗ ╚██████╗ ███████╗ ███████║ ███████║    ██╗"
 echo "  ╚══════╝  ╚═════╝   ╚═════╝  ╚═════╝ ╚══════╝ ╚══════╝ ╚══════╝    ╚═╝"
 tput sgr0
-echo; echo; 
-echo "Open AI Gym setup complete."
+echo
+echo "OpenAI Gym setup complete."
 echo "Use 'import gym' to use Gym in python files"
-echo "Run 'source activate p36' on command line to enter python 3.6 environment"
-echo "You can uninstall with 'pip uninstall gym'"
+echo
+echo "To rerun the example agent, enter these commands in terminal:"
+echo "    source ~/.bash_profile"
+echo "    source activate p35"
+echo "    cd ${dir}"
+echo "    python example_agent.py"
 echo
 echo "For next steps, check out the Gym docs"
-echo
 echo "https://gym.openai.com/docs"
 echo
